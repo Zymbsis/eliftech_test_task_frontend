cabed274c07f3be531a0d1d82ac0011c0176231d
Zymbsis <aleksandra.aleksashka1809@gmail.com>
2024-09-24 17:37:49 +0200

    min fix

--- src/helpers/sortOptionsList.js
+++ src/helpers/sortOptionsList.js
@@ -7,14 +7,14 @@ export const sortOptionsList = [
     value: { sortBy: 'event_date', sortOrder: 'asc' },
     label: 'Event date (A to Z)',
   },
-  { value: { sortBy: 'title', sortOrder: 'desc' }, label: 'Title (Z to A)' },
   { value: { sortBy: 'title', sortOrder: 'asc' }, label: 'Title (A to Z)' },
-  {
-    value: { sortBy: 'organizer', sortOrder: 'desc' },
-    label: 'Organizer (Z to A)',
-  },
+  { value: { sortBy: 'title', sortOrder: 'desc' }, label: 'Title (Z to A)' },
   {
     value: { sortBy: 'organizer', sortOrder: 'asc' },
     label: 'Organizer (A to Z)',
+  },
+  {
+    value: { sortBy: 'organizer', sortOrder: 'desc' },
+    label: 'Organizer (Z to A)',
   },
 ];
